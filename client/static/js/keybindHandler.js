79a0123737f4782be31e824c28b5dcf6a56c23d6
Joshua Bharathi <blitz04.dev@gmail.com>
2022-05-04 17:13:52 +0530

    moved ArrowLeft and ArrowRight out of Alt Group

--- client/static/js/keybindHandler.js
+++ client/static/js/keybindHandler.js
@@ -17,6 +17,10 @@ export class KeyBindHandler {
             this.player.resume();
         } else if (this.keybinds["l"]) {
             this.player.islooping = !this.player.islooping;
+        } else if (this.keybinds["ArrowLeft"]) {
+            this.player.audio.currentTime -= 5;
+        } else if (this.keybinds["ArrowRight"]) {
+            this.player.audio.currentTime += 5;
         } else if (this.keybinds["Alt"]) {
             if (this.keybinds["ArrowUp"]) {
                 // TODO: display current volume
@@ -26,11 +30,7 @@ export class KeyBindHandler {
             } else if (this.keybinds["ArrowDown"]) {
                 e.preventDefault();
                 this.player.setVolume(this.player.audio.volume - 0.1);
-            } else if (this.keybinds["ArrowLeft"]) {
-                this.player.audio.currentTime -= 5;
-            } else if (this.keybinds["ArrowRight"]) {
-                this.player.audio.currentTime += 5;
-            }
+            } 
         }
     }
 }
